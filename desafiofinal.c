ca18a0612d6d92a8abb2a7e3fb88da03a4b6007e
Mateus Sampaio <mateussampaio2710@gmail.com>
2025-04-06 20:33:46 -0300

    primeiro commit

---
+++ desafiofinal.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+//Jogom da Adivinhação Numerica
+int main(){ 
+    int num, chute;
+        printf("Pense em um numero de 1 a 1023\n");
+        printf("Agora me diga ele para que o programa tente adivinhar:\n");
+        scanf("%d", &num);
+        while(1){
+            int min =1 , max = 1023;
+            for (int i = 0; i < 10; i++){
+                
+                chute = (min + max)/2;
+                char sinal;
+                printf("O seu numero e >, < ou = a %d\n", chute );
+                scanf("%s", &sinal);
+                if(sinal == '='){
+                    printf("adivinhei o numero %d em %d tentativas\n", chute, i + 1);
+                    break;
+                }
+                else if (sinal == '<'){
+                  max = chute -1; 
+                }
+                else if(sinal =='>'){
+                  min = chute + 1;
+
+                }
+                if(i == 10){
+                    printf("nao consegui adivinhar\n");
+                }  
+            }
+            printf("Pense em um numero de 1 a 1023\n");
+            printf("Agora me diga ele para que o programa tente adivinhar:(Flag numero menor que 1)\n");
+            scanf("%d", &num);
+            if (num<1)
+            {
+               break;
+            }  
+        }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+// Desafio final Linguagem C //
+Nome: Mateus Sampaio Luz Silva
+
+
+Jogo da Adivinhação Numérica 
+
+Bem-vindo ao desafio final: o jogo da adivinhação numérica! Crie um programa que desafie o usuário a pensar em um conjunto de números (flag valor menor que 1), entre 1 e 1023. Seu programa deve adivinhar para cada numero escolhido em no máximo 10 tentativas, usando a técnica do valor intermediário. Conte quantas tentativas foram necessárias para alcançar a vitória para cada numero.
